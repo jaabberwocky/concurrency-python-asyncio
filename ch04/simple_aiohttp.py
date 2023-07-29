e32f79ebe6cd6ea093258ec954f53df43da163c1
tobias <tobias@tobias-leong.dev>
2023-07-30 00:43:39 +0800

    Refactor simple_aiohttp.py with tasks

--- ch04/simple_aiohttp.py
+++ ch04/simple_aiohttp.py
@@ -7,15 +7,22 @@ from util import async_timed
 @async_timed()
 async def fetch_status(session: ClientSession, url: str) -> int:
     async with session.get(url) as result:
-        return result.status
+        return (url, result.status)
 
 
 @async_timed()
 async def main():
     async with aiohttp.ClientSession() as session:
-        url = 'https://www.example.com'
-        status = await fetch_status(session, url)
-        print(f'Status for {url} was {status}')
+        urls = ['https://www.example.com',
+                'https://www.google.com', 'https://www.yahoo.com']
+        tasks = []
+        for url in urls:
+            print(f"creating task for {url}")
+            tasks.append(asyncio.create_task(fetch_status(session, url)))
 
+        for task in tasks:
+            url, status = await task
+            print(f'Status for {url} was {status}')
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
