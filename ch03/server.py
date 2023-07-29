5a563278f020743f49bda7e0fe727dbdc885a5e5
tobias <tobias@tobias-leong.dev>
2023-07-29 11:23:15 +0800

    Refactor server code for asyncio event loop and graceful exit
    - Refactored server code to use `asyncio` and `AbstractEventLoop` for event loop management and asynchronous socket operations.
    - Added `echo` coroutine to handle incoming data and send responses.
    - Implemented `connection_listener` coroutine to accept incoming connections and create `echo` tasks for each connection.
    - Added `GracefulExit` class to handle graceful termination of server.
    - Implemented `shutdown` function and `close_echo_tasks` coroutine to gracefully stop and close all `echo` tasks.
    - Updated `main` function to handle signals and run the event loop until completion or graceful exit occurs.

--- ch03/server.py
+++ ch03/server.py
@@ -1,36 +1,73 @@
-import selectors
+import asyncio
+from asyncio import AbstractEventLoop
 import socket
-from selectors import SelectorKey
-from typing import List, Tuple
+import logging
+import signal
+from typing import List
 
-selector = selectors.DefaultSelector()
+
+async def echo(connection: socket,
+               loop: AbstractEventLoop) -> None:
+    try:
+        while data := await loop.sock_recv(connection, 1024):
+            print('got data!')
+            if data == b'boom\r\n':
+                raise Exception("Unexpected network error")
+            await loop.sock_sendall(connection, data)
+    except Exception as ex:
+        logging.exception(ex)
+    finally:
+        connection.close()
+
+echo_tasks = []
 
 
-server_socket = socket.socket()
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+async def connection_listener(server_socket, loop):
+    while True:
+        connection, address = await loop.sock_accept(server_socket)
+        connection.setblocking(False)
+        print(f"Got a connection from {address}")
+        echo_task = asyncio.create_task(echo(connection, loop))
+        echo_tasks.append(echo_task)
 
-server_address = ("127.0.0.1", 8000)
-server_socket.setblocking(False)
-server_socket.bind(server_address)
-server_socket.listen()
 
-selector.register(server_socket, selectors.EVENT_READ)
+class GracefulExit(SystemExit):
+    pass
 
-while True:
-    events: List[Tuple[SelectorKey, int]] = selector.select(timeout=1)
 
-    if len(events) == 0:
-        print("No events, waiting a bit more!")
+def shutdown():
+    raise GracefulExit()
 
-    for event, _ in events:
-        event_socket = event.fileobj
 
-        if event_socket == server_socket:  # this means connection attempt
-            connection, address = server_socket.accept()
-            connection.setblocking(False)
-            print(f"I got a connection from {address}")
-            selector.register(connection, selectors.EVENT_READ)
-        else:
-            data = event_socket.recv(1024)
-            print(f"I got some data: {data}")
-            event_socket.send(f"echo: {data}\n".encode())
+async def close_echo_tasks(echo_tasks: List[asyncio.Task]):
+    waiters = [asyncio.wait_for(task, 2) for task in echo_tasks]
+    for task in waiters:
+        try:
+            await task
+        except asyncio.exceptions.TimeoutError:
+            # We expect a timeout error here
+            pass
+
+
+async def main():
+    server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = ('127.0.0.1', 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+
+    for signame in {'SIGINT', 'SIGTERM'}:
+        loop.add_signal_handler(getattr(signal, signame), shutdown)
+    await connection_listener(server_socket, loop)
+
+
+loop = asyncio.new_event_loop()
+
+try:
+    loop.run_until_complete(main())
+except GracefulExit:
+    loop.run_until_complete(close_echo_tasks(echo_tasks))
+finally:
+    loop.close()
