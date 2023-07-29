032a4e743e64b2b99de06753d42e8f96904a1136
tobias <tobias@tobias-leong.dev>
2023-07-30 00:51:04 +0800

    Refactor simple_aiohttp.py with timeout through wait_for

--- ch04/simple_aiohttp.py
+++ ch04/simple_aiohttp.py
@@ -6,6 +6,10 @@ from util import async_timed
 
 @async_timed()
 async def fetch_status(session: ClientSession, url: str) -> int:
+    # simulating a very slow request which will be timed-out
+    if url == "https://www.youtube.com":
+        print("sleeping...")
+        await asyncio.sleep(10)
     async with session.get(url) as result:
         return (url, result.status)
 
@@ -14,14 +18,17 @@ async def fetch_status(session: ClientSession, url: str) -> int:
 async def main():
     async with aiohttp.ClientSession() as session:
         urls = ['https://www.example.com',
-                'https://www.google.com', 'https://www.yahoo.com']
+                'https://www.google.com', 'https://www.yahoo.com', 'https://www.youtube.com']
         tasks = []
         for url in urls:
             print(f"creating task for {url}")
             tasks.append(asyncio.create_task(fetch_status(session, url)))
 
         for task in tasks:
-            url, status = await task
+            try:
+                url, status = await asyncio.wait_for(task, 2)
+            except asyncio.exceptions.TimeoutError:
+                print(f"Timeout!")
             print(f'Status for {url} was {status}')
 
 if __name__ == '__main__':
